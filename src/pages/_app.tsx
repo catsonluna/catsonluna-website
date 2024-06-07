eba71a3c84fb1a37ce2877c5f125b2e7b2635a76
luna <56201697+catsonluna@users.noreply.github.com>
2024-06-08 01:17:06 +0300

    verel anal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import "@/styles/globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 import { Nunito } from "next/font/google"
 
@@ -14,6 +15,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <main className={font.className}>
       <SpeedInsights />
+      <Analytics />
       <Component {...pageProps} />
     </main>
   );
